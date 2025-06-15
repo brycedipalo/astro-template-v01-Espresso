f641fb74c8392e758fd994d6150547237477b59d
Bryce <bdipalo@hotmail.com>
2025-06-15 09:15:39 -0700

    update config for gihub pages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
+  base: '/astro-template-v01-Espresso/', // replace with your GitHub repo name
+  output: 'static',
 });
+
